22e19f64907d9909a0cebe9e2172d65e8c97c73d
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-10-26 10:00:56 +0530

    Send User compleated

--- frontend/src/components/Otherusers.jsx
+++ frontend/src/components/Otherusers.jsx
@@ -1,26 +1,27 @@
-import React from "react";
-import Otheruser from "./Otheruser";
-import useGetOtherUsers from "../hooks/useGetOtherUser";
-import { useSelector } from "react-redux";
+import React from 'react'
+import OtherUser from './Otheruser';
+import useGetOtherUsers from '../hooks/useGetOtherUser';
+import {useSelector} from "react-redux";
 
-const Otherusers = () => {
 
-  useGetOtherUsers();
-  const {otherUsers} = useSelector(store=> store.user);
-  if(!otherUsers) return; // early return in react
+const OtherUsers = () => {
+    // my custom hook
+    useGetOtherUsers();
+    const {otherUsers} = useSelector(store=>store.user);
+    if (!otherUsers) return; // early return in react
+     
+    return (
+        <div className='overflow-auto flex-1'>
+            {
+                otherUsers?.map((user)=>{
+                    return (
+                        <OtherUser key={user._id} user={user}/>
+                    )
+                })
+            }
+            
+        </div>
+    )
+}
 
-  return (
-    <div className="overflow-auto flex-1">
-        {
-          otherUsers?.map((user)=> {
-            return(
-              <Otheruser key={user._id} user={user}/>
-            )
-          })
-        }
-
-    </div>
-  );
-};
-
-export default Otherusers;
+export default OtherUsers
