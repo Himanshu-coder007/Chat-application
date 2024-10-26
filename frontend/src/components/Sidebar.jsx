87b4bf6d515777cc245c0ff0e9ac64f1688e559a
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-10-26 11:20:00 +0530

    searc user compleated

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
 import Otherusers from "./Otherusers";
 import axios from "axios";
 import toast from "react-hot-toast";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setOtherUsers } from "../redux/userSlice";
+
 
 const Sidebar = () => {
+  const [search, setSearch] = useState("");
+  const {otherUsers} = useSelector(store=>store.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const logoutHandler = async () => {
     try {
-        const res = await axios.get(`http://localhost:8080/api/v1/user/logout`);
-        navigate("/login");
-        toast.success(res.data.message);
+      const res = await axios.get(`http://localhost:8080/api/v1/user/logout`);
+      navigate("/login");
+      toast.success(res.data.message);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-}
-
+  };
+  const searchSubmitHandler = (e) => {
+    e.preventDefault();
+    const conversationUser = otherUsers?.find((user)=>user.fullName.toLowerCase().includes(search.toLowerCase()));
+    if(conversationUser){
+      dispatch(setOtherUsers([conversationUser]));
+    } else{
+      toast.error("User not found");
+    }
+  };
   return (
     <div className="border-slate-500 p-4 flex flex-col">
-      <form action="" className="flex items-center gap-2">
+      <form
+        onSubmit={searchSubmitHandler}
+        action=""
+        className="flex items-center gap-2"
+      >
         <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           className="input input-bordered rounded-md"
           type="text"
           placeholder="Search..."
