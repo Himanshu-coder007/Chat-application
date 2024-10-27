fec01e57d626be54036b208e14ce250d752a7a13
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-10-27 21:07:53 +0530

    working on socket.io

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,11 @@ import Signup from './components/Signup';
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import io from "socket.io-client";
+
+
 
 const router = createBrowserRouter([
   { path: "/", element: <Homepage /> },
@@ -11,6 +16,22 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [socket,setSocket] = useState(null);
+  const {authUser} = useSelector(store=>store.user);
+ 
+
+  useEffect(()=>{
+    if(authUser){
+      const socket = io(`http://localhost:8080`, {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      setSocket(socket);
+    }
+  },[authUser]);
+
+
   return (
     <div className="p-4 h-screen flex items-center justify-center">
      <RouterProvider router={router}/>
