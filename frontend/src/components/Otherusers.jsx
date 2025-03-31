83f11c2cf05818b2947a1f01281b2869936e448d
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-04-01 00:41:34 +0530

    some issues fixed

--- frontend/src/components/Otherusers.jsx
+++ frontend/src/components/Otherusers.jsx
@@ -3,25 +3,30 @@ import OtherUser from './Otheruser';
 import useGetOtherUsers from '../hooks/useGetOtherUser';
 import {useSelector} from "react-redux";
 
-
 const OtherUsers = () => {
-    // my custom hook
     useGetOtherUsers();
-    const {otherUsers} = useSelector(store=>store.user);
-    if (!otherUsers) return; // early return in react
-     
+    const {otherUsers} = useSelector(store => store.user);
+    
+    // Better null/undefined check and ensure it's an array
+    if (!Array.isArray(otherUsers)) {
+        return (
+            <div className='overflow-auto flex-1'>
+                <p>No users found or loading...</p>
+            </div>
+        );
+    }
+
     return (
         <div className='overflow-auto flex-1'>
-            {
-                otherUsers?.map((user)=>{
-                    return (
-                        <OtherUser key={user._id} user={user}/>
-                    )
-                })
-            }
-            
+            {otherUsers.length > 0 ? (
+                otherUsers.map((user) => (
+                    <OtherUser key={user._id} user={user} />
+                ))
+            ) : (
+                <p>No other users available</p>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default OtherUsers
+export default OtherUsers;
