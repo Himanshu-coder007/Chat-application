68861a54318615ae474cb0af30305cc0fba939ff
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-04-09 23:06:32 +0530

    backend updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,6 +72,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "none",
+        secure: true,
       })
       .json({
         _id: user._id,
